28dae2619f74f76f9b68a83f5f4e3715574dd10f
cobalt2727 <60624944+cobalt2727@users.noreply.github.com>
2022-10-27 19:21:02 -0400

    switch to only displaying whether the washer is on or off

--- mqtt-test-power-consumption.sh
+++ mqtt-test-power-consumption.sh
@@ -6,18 +6,25 @@ if [ -v KASA_CURRENT_IP ]; then
   echo "Attempting to connect to $KASA_CURRENT_IP..."
 
   echo "You won't see any output in the terminal, but check the testKasaOutput.txt file for results as it progresses. When you're ready to stop this script, press Ctrl+C at any time."
-  echo "Time,Power" >> testKasaOutput.csv
+  echo "Time,Power" >>testKasaOutput.csv
   while true; do
     # currentTime=$(date +%s.%N)
     currentTime=$(echo "$(date +%s.%N) $KASA_SCRIPT_START_TIME" | awk '{print $1 - $2}')
-    echo -n "$currentTime," >> testKasaOutput.csv
+    echo -n "$currentTime," >>testKasaOutput.csv
 
     # kasa --type plug --host $KASA_CURRENT_IP emeter | grep Power || (echo -e "\e[31mConnection lost! Check the testKasaOutput.txt file for results up to this point. \e[0m" && exit 1)
     power=$(kasa --type plug --host $KASA_CURRENT_IP emeter | grep Power | awk '{print $2}' || echo -e "\e[31mConnection lost! Check the testKasaOutput.txt file for results up to this point. \e[0m")
-    echo $power >> testKasaOutput.csv
+    echo $power >>testKasaOutput.csv
 
     # publishing to mqtt broker
-    mosquitto_pub -h test.mosquitto.org -t washer -m "Time: $currentTime, Power: $power"
+    # mosquitto_pub -h test.mosquitto.org -t washer -m "Time: $currentTime, Power: $power"
+    if (($(echo "$power 9" | awk '{print ($1 > $2)}'))); then
+      mosquitto_pub -h test.mosquitto.org -t washer -m "On"
+      # echo "ON"
+    else
+      mosquitto_pub -h test.mosquitto.org -t washer -m "Off"
+      # echo "OFF"
+    fi
 
     # sleep 0.1
   done

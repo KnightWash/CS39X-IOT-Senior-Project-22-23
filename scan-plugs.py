5a5d8dcc2997ba9a3dbd8bf3656e45571bc17921
aayam-shrestha <aayam2071@gmail.com>
2023-01-15 16:48:38 -0500

    python rewrite working

--- scan-plugs.py
+++ scan-plugs.py
@@ -15,51 +15,63 @@ import csv
 
 
 async def main():
+    oneRunBefore = False
+    twoRunsBefore = False
 
     plugAddresses = open("addresses.txt", "r")
-    plugList = plugAddresses.readlines()
+    scanList = plugAddresses.readlines()
+    plugList = [p.strip() for p in scanList]
     startTime = time.time()
     plugAddresses.close()
 
 
     while(True):
         print("hello world")
-
+        print(plugList)
         for plug in plugList:
-            log_file = open("testKasaOutput.csv", "a+")
+            print(plug)
+            # log_file = open("testKasaOutput.csv", "a+")
 
             currentPlug = SmartPlug(plug)
             # currentTime=$(echo "$(date +%s.%N) $KASA_SCRIPT_START_TIME" | awk '{print $1 - $2}')
             currentTime = (startTime - time.time())
 
-            # echo -n "$currentTime," >>testKasaOutput.csv
-            csv.writer(log_file).writerow(currentTime)
+            # csv.writer(log_file).writerow(currentTime)
 
-            # # kasa --type plug --host $KASA_CURRENT_IP emeter | grep Power || (echo -e "\e[31mConnection lost! Check the testKasaOutput.txt file for results up to this point. \e[0m" && exit 1)
-            # power=$(kasa --type plug --host $KASA_CURRENT_IP emeter | grep Power | awk '{print $2}' || echo -e "\e[31mConnection lost! Check the testKasaOutput.txt file for results up to this point. \e[0m")
-            # echo $power >>testKasaOutput.csv
-            await plug.update()  # Request an update
-            print(plug.emeter_realtime)
-
+            await currentPlug.update()  # Request an update
+            eMeterCheck = currentPlug.emeter_realtime
+            # let's pull the actual number we want out of eMeterCheck
+            powerLevel = float(str(eMeterCheck).split("=", 1)[1].split(" ", 1)[0])
+            print(powerLevel)
 
             # # publishing to mqtt broker
-            # # mosquitto_pub -h test.mosquitto.org -t washer -m "Time: $currentTime, Power: $power"
             client = mqtt.Client("Beta")
             client.connect("test.mosquitto.org")
-            client.publish(currentPlug.alias, "testing")
-            
+            print("Publishing to " + currentPlug.alias)
 
-            # echo "publishing to $plugName"
-            # if (($(echo "$power 9" | awk '{print ($1 > $2)}'))); then
-            #   mosquitto_pub -h test.mosquitto.org -t $plugName -m "On" || (echo -e "\e[31mERROR: $KASA_CURRENT_IP failed at $(date +%s.%N) \e[0m" && exit 1)
-            #   # echo "ON"
-            # else
-            #   mosquitto_pub -h test.mosquitto.org -t $plugName -m "Off" || (echo -e "\e[31mERROR: $KASA_CURRENT_IP failed at $(date +%s.%N) \e[0m" && exit 1)
-            #   # echo "OFF"
-            # fi
+            # Only publish on state change
+            if powerLevel > 11:
+                currentRun = True
+                if currentRun == oneRunBefore == twoRunsBefore:
+                    # do nothing
+                    continue
+                else:
+                    print("print 'On' to mqtt here")
+                    client.publish(currentPlug.alias, "On")
+            else:
+                currentRun = False
+                if currentRun == oneRunBefore == twoRunsBefore:
+                    # do nothing
+                    continue
+                else:
+                    print("print 'Off' to mqtt here")
+                client.publish(currentPlug.alias, "Off")
         
             #don't put anything after this line in the for loop - we need it to save every plug's ping data
-            log_file.close()
+            # log_file.close()
+
+        twoRunsBefore = oneRunBefore
+        oneRunBefore = powerLevel
 
 if __name__ == "__main__":
     asyncio.run(main())

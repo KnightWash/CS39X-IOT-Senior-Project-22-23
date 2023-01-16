11e5eaa35e0903c68fdf799504aeb5992375c626
kwietelmann <kaw57@calvin.edu>
2023-01-15 19:07:25 -0500

    python rewrite part 2

--- scan-plugs.py
+++ scan-plugs.py
@@ -13,65 +13,92 @@ import csv
 # print(plug_1.emeter_realtime) # Print out current emeter status
 
 
+class LaundryMachine:
+    def __init__(plug):
+        plug.twoRunsBefore = False
+        plug.oneRunBefore = False
+
 
 async def main():
-    oneRunBefore = False
-    twoRunsBefore = False
 
     plugAddresses = open("addresses.txt", "r")
     scanList = plugAddresses.readlines()
-    plugList = [p.strip() for p in scanList]
-    startTime = time.time()
     plugAddresses.close()
+    # strip newlines out of the list of plugs from the document...
+    plugList = [p.strip() for p in scanList]
+    # startTime = time.time()
 
+    # for plug in plugList:
+    #     client = mqtt.Client("Beta")
+    #     client.connect("test.mosquitto.org")
+
+    #     currentPlug = SmartPlug(plug)
+    #     await currentPlug.update()  # Request an update
+
+    #     client.publish(currentPlug.alias, "Off")
+
+    # for plug in plugList:
+    #     plugHistory = LaundryMachine(plug)
+    #     print(plugHistory.oneRunBefore)
+    #     print(plugHistory.twoRunsBefore)
+
+    objs = [LaundryMachine() for i in plugList]
+    for obj in objs:
+        obj.oneRunBefore = False
+        obj.twoRunsBefore = False
+
+    print(objs[0].oneRunBefore)
+    print(objs[0].twoRunsBefore)
+    print(objs)
 
     while(True):
         print("hello world")
-        print(plugList)
+        # print(plugList)
+
         for plug in plugList:
-            print(plug)
             # log_file = open("testKasaOutput.csv", "a+")
 
             currentPlug = SmartPlug(plug)
-            # currentTime=$(echo "$(date +%s.%N) $KASA_SCRIPT_START_TIME" | awk '{print $1 - $2}')
-            currentTime = (startTime - time.time())
+            # currentTime = (startTime - time.time())
 
             # csv.writer(log_file).writerow(currentTime)
 
             await currentPlug.update()  # Request an update
+            print(currentPlug.alias + "'s power level is...")
             eMeterCheck = currentPlug.emeter_realtime
             # let's pull the actual number we want out of eMeterCheck
-            powerLevel = float(str(eMeterCheck).split("=", 1)[1].split(" ", 1)[0])
+            powerLevel = float(str(eMeterCheck).split(
+                "=", 1)[1].split(" ", 1)[0])
             print(powerLevel)
 
             # # publishing to mqtt broker
             client = mqtt.Client("Beta")
             client.connect("test.mosquitto.org")
-            print("Publishing to " + currentPlug.alias)
+            # print("Publishing to " + currentPlug.alias)
 
             # Only publish on state change
             if powerLevel > 11:
                 currentRun = True
                 if currentRun == oneRunBefore == twoRunsBefore:
-                    # do nothing
-                    continue
-                else:
                     print("print 'On' to mqtt here")
                     client.publish(currentPlug.alias, "On")
+                else:
+                    # do nothing
+                    continue
             else:
                 currentRun = False
                 if currentRun == oneRunBefore == twoRunsBefore:
+                    print("print 'Off' to mqtt here")
+                    client.publish(currentPlug.alias, "Off")
+                else:
                     # do nothing
                     continue
-                else:
-                    print("print 'Off' to mqtt here")
-                client.publish(currentPlug.alias, "Off")
-        
-            #don't put anything after this line in the for loop - we need it to save every plug's ping data
+
+            # don't put anything after this line in the for loop - we need it to save every plug's ping data
             # log_file.close()
 
-        twoRunsBefore = oneRunBefore
-        oneRunBefore = powerLevel
+            twoRunsBefore = oneRunBefore
+            oneRunBefore = currentRun
 
 if __name__ == "__main__":
     asyncio.run(main())

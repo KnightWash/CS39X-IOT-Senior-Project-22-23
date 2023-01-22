606904052f7213b231604456f8bd0db19e7f7919
kwietelmann <kaw57@calvin.edu>
2023-01-21 19:15:35 -0500

    Python: quick temporary scan fail crash fix

--- scan-plugs.py
+++ scan-plugs.py
@@ -57,41 +57,47 @@ async def main():
 
             # csv.writer(log_file).writerow(currentTime)
 
-            await currentPlug.update()  # Request an update
-            print(currentPlug.alias + "'s power level is...")
-            eMeterCheck = currentPlug.emeter_realtime
-            # let's pull the actual number we want out of eMeterCheck
-            powerLevel = float(str(eMeterCheck).split(
-                "=", 1)[1].split(" ", 1)[0])
-            print(powerLevel)
-
-            # # publishing to mqtt broker
-            client = mqtt.Client("Beta")
-            client.connect(MQTTServerName)
-
-            # Only publish on state change
-            if powerLevel > 11:
-                plug.currentRun = 0
-                if plug.currentRun != plug.previousMachineState:
-                    if plug.currentRun == plug.oneRunBefore == plug.twoRunsBefore:
-                        plug.previousMachineState = 0
-                        print("print 'On' to mqtt here")
-                        client.publish(currentPlug.alias,
-                                       payload="On", retain=True)
+            try:
+                await currentPlug.update()  # Request an update
+            except:
+                print("WARNING: SCAN FAILED FOR " + currentPlug.alias + "...")
             else:
-                plug.currentRun = 1
-                if plug.currentRun != plug.previousMachineState:
-                    if plug.currentRun == plug.oneRunBefore == plug.twoRunsBefore:
-                        plug.previousMachineState = 1
-                        print("print 'Off' to mqtt here")
-                        client.publish(currentPlug.alias,
-                                       payload="Off", retain=True)
+                print(currentPlug.alias + "'s power level is...")
+                eMeterCheck = currentPlug.emeter_realtime
+                # let's pull the actual number we want out of eMeterCheck
+                powerLevel = float(str(eMeterCheck).split(
+                    "=", 1)[1].split(" ", 1)[0])
+                print(powerLevel)
 
-            # don't put anything after this line in the for loop - we need it to save every plug's ping data
-            # log_file.close()
+                # # publishing to mqtt broker
+                client = mqtt.Client("Beta")
+                client.connect(MQTTServerName)
 
-            plug.twoRunsBefore = plug.oneRunBefore
-            plug.oneRunBefore = plug.currentRun
+                # Only publish on state change
+                if powerLevel > 11:
+                    plug.currentRun = 0
+                    if plug.currentRun != plug.previousMachineState:
+                        if plug.currentRun == plug.oneRunBefore == plug.twoRunsBefore:
+                            plug.previousMachineState = 0
+                            print("print 'On' to mqtt here")
+                            client.publish(currentPlug.alias,
+                                           payload="On", retain=True)
+                else:
+                    plug.currentRun = 1
+                    if plug.currentRun != plug.previousMachineState:
+                        if plug.currentRun == plug.oneRunBefore == plug.twoRunsBefore:
+                            plug.previousMachineState = 1
+                            print("print 'Off' to mqtt here")
+                            client.publish(currentPlug.alias,
+                                           payload="Off", retain=True)
+
+                # don't put anything after this line in the for loop - we need it to save every plug's ping data
+                # log_file.close()
+
+                plug.twoRunsBefore = plug.oneRunBefore
+                plug.oneRunBefore = plug.currentRun
+            finally:
+                print("=============================================")
 
 if __name__ == "__main__":
     asyncio.run(main())

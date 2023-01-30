99152d062ffa21f3494e18a9262cedc6365c9059
kwietelmann <90988174+kwietelmann@users.noreply.github.com>
2023-01-30 18:49:50 -0500

    fix tester name/"location"

--- archive/10-second-tester.sh
+++ archive/10-second-tester.sh
@@ -1,2 +1,2 @@
-mosquitto_pub -h test.mosquitto.org -t calvin/beta/washer/test -m "On"
-watch -n10 'mosquitto_pub -h test.mosquitto.org -t calvin/beta/washer/test -m "Off" && sleep 10 && mosquitto_pub -h test.mosquitto.org -t calvin/beta/washer/test -m "On"'
+mosquitto_pub -h test.mosquitto.org -t calvin/test/machine/location -m "On"
+watch -n10 'mosquitto_pub -h test.mosquitto.org -t calvin/test/machine/location -m "Off" && sleep 10 && mosquitto_pub -h test.mosquitto.org -t calvin/test/machine/location -m "On"'

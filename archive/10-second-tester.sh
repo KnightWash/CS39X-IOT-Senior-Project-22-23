421f24fe27203e72c1a43e66e4af2c73df127738
kwietelmann <90988174+kwietelmann@users.noreply.github.com>
2023-01-30 19:02:41 -0500

    make tester a dryer

--- archive/10-second-tester.sh
+++ archive/10-second-tester.sh
@@ -1,2 +1,2 @@
-mosquitto_pub -h test.mosquitto.org -t calvin/test/washer/location -m "On"
-watch -n10 'mosquitto_pub -h test.mosquitto.org -t calvin/test/washer/location -m "Off" && sleep 10 && mosquitto_pub -h test.mosquitto.org -t calvin/test/washer/location -m "On"'
+mosquitto_pub -h test.mosquitto.org -t calvin/test/dryer/location -m "On"
+watch -n10 'mosquitto_pub -h test.mosquitto.org -t calvin/test/dryer/location -m "Off" && sleep 10 && mosquitto_pub -h test.mosquitto.org -t calvin/test/dryer/location -m "On"'
